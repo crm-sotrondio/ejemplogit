cb8169c05aa4de2e63fa6b3d2596f196363060e7
xccas <xccas@192.168.3.16>
2024-01-06 01:32:59 +0100

    segundo commit

--- Ejemplo1/src/main/Main.java
+++ Ejemplo1/src/main/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+   System.out.println("Hola mundo");
+   
+   System.out.println("Prueba segundo commit github");
 	}
 
 }
